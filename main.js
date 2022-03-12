41b73a75f85712f2deb401ba36782a8ef4dac288
Rony Stico <ronystico@gmail.com>
2022-03-12 00:01:52 -0400

    Images are now added via Javascript

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Java script
+for(let i = 1;i<=3; i++){
+    const imgtag = document.createElement("img");
+    imgtag.id = `imagen${i}`;
+    imgtag.alt = `imagen${i}`;
+    imgtag.src = `img/imagen${i}.jpg`;
+    document.querySelector(".grid").appendChild(imgtag);
+}
